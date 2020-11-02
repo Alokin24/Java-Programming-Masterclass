05035057d0ffbf23f6da795b87a15acec3fc705e
Nikola Anastasovski <Nikola.Anastasovski@netcetera.com>
2020-11-02 14:34:55 +0100

    Initial commit

---
+++ src/OOP1/poolarea/Cuboid.java
@@ -0,0 +1,20 @@
+package OOP1.poolarea;
+
+public class Cuboid extends Rectangle {
+    private double width;
+    private double length;
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = (height < 0) ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return height * getArea();
+    }
+}
